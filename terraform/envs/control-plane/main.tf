5741377a5f9136fb8220d2184acd3b424d1f74fb
Joseph Minkoulou (JDA PARTNERS TECHNICAL SERVICES) <v-jminkoulou@microsoft.com>
2026-04-23 18:55:57 -0500

    Module 1: Azure RG and Storage Account via CI/CD

--- terraform/envs/control-plane/main.tf
+++ terraform/envs/control-plane/main.tf
@@ -2,16 +2,37 @@ terraform {
   required_version = ">= 1.6.0"
 
   required_providers {
-    null = {
-      source = "hashicorp/null"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
 
-provider "null" {}
+provider "azurerm" {
+  features {}
+}
 
-resource "null_resource" "control_plane" {
-  triggers = {
-    message = "CI/CD is the only execution plane"
-  }
+resource "azurerm_resource_group" "nonprod" {
+  name     = "rg-platform-nonprod"
+  location = "eastus"
+}
+
+resource "random_string" "suffix" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "platform" {
+  name                            = "plat${random_string.suffix.result}"
+  resource_group_name             = azurerm_resource_group.nonprod.name
+  location                        = azurerm_resource_group.nonprod.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
 }
